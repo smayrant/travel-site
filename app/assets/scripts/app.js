48238b158a7cfe99732a61839bacee25713ff2b3
smayrant <35153929+smayrant@users.noreply.github.com>
2018-06-09 05:56:19 -0400

    Delete app.js

--- app/assets/scripts/app.js
+++
@@ -1,9 +0,0 @@
-var person = {
-  name: 'Rick Mayrant',
-  favColor: 'blue',
-  greeting: function (){
-    console.log(`Hello, my name is ${this.name} and my favorite color is ${this.favColor}`);
-  }
-}
-
-person.greeting();
